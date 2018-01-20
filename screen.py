4e10f3bb67ae3c5e1dda9885cad26b38e6c1e875
fuzzylemur <giladam@gmail.com>
2018-01-20 18:16:02 +0200

    working!

--- screen.py
+++ screen.py
@@ -56,15 +56,17 @@ class Screen:
         """
         :return:
         """
-        label_bg = tk.Label(self.__root, image=self.__bg, bd=0, cursor='cross')
+        self.__root.resizable(width=False, height=False)
+
+        label_bg = tk.Label(self.__root, image=self.__bg, bd=0)
         label_bg.image = self.__bg
         label_bg.pack()
 
         for i in range(Game.BOARD_Y):
             temp_row = []
             for j in range(Game.BOARD_X):
-                temp_label = tk.Label(self.__root, image=self.__coin1,  bd=0)
-                temp_label.image = self.__coin1
+                temp_label = tk.Label(self.__root, image=self.__blank,  bd=0)
+                temp_label.image = self.__blank
                 temp_label.place(x=self.CELL_ORI[0]+j*self.OFF[0], y=self.CELL_ORI[1]+i*self.OFF[1])
                 temp_row.append(temp_label)
             self.__cells.append(temp_row)
@@ -180,10 +182,11 @@ class Screen:
         """
         pass
 
-"""
+
 def func(x):
     pass
 
+"""
 root = tk.Tk()
 screen = Screen(root, func)
 screen.win((0,0),(1,0),1)
