116ed7c1f4aed08ded6babe7fed01f7c72a5b88f
jzedaka <35489421+jzedaka@users.noreply.github.com>
2018-01-20 23:32:52 +0200

    Add files via upload

--- ai.py
+++ ai.py
@@ -1,6 +1,7 @@
 from random import sample
 from copy import deepcopy
 from game import Game
+#import time
 
 class Node:
 
@@ -33,39 +34,55 @@ class AI:
     def __init__(self, player):
         self.__player = player
         self.__cur_node = Node()
+        self.__next_move = None
 
     def find_legal_move(self, g, func, timeout=None):
         """"""
-        for i in range(7):
-            try:
+        #timer = time.time()
+        # Saves random move as our move 
+        # and then improves it until the timeout
+        legal_moves = set([move for move in range(7) if not g.is_col_full(move)])
+        self.__next_move = sample(legal_moves, 1)[0]
+
+        try:
+            # look for a winning move(depth 1)
+            for i in legal_moves:
+                temp = g.get_last_coord()
                 g.make_move(i)
                 if g.get_winner() == self.__player:
-                    g.unmake_move()
-                    func(i)
+                    g.unmake_move(i, temp)
+                    best_final_move = i
                     return
                 else:
-                    g.unmake_move()
+                    g.unmake_move(i, temp)
 
-            except:
-                continue
+            last_col = g.get_last_coord()
+            if last_col is not None and self.__cur_node.get_children() != dict():
+                children = self.__cur_node.get_children()
+                self.__cur_node = children[last_col[1]]
 
-        last_col = g.get_last_coord()
-        if last_col is not None and self.__cur_node.get_children() != dict():
-            children = self.__cur_node.get_children()
-            self.__cur_node = children[last_col[1]]
+            self.build_tree(g, self.__cur_node)
 
-        self.build_tree(g, self.__cur_node)
-        children = self.__cur_node.get_children()
-        best_move = max(children.keys(), key=lambda k: children[k].get_data())
-        self.__cur_node = children[best_move]
-        func(best_move)
 
+        finally:
+            self.__cur_node = self.__cur_node.get_children()[self.__next_move]
+            #returns the best move we found so far
+            func(self.__next_move)
+    
+    def set_next_best_move(self, children):
+        """"""
+        self.__next_move = max(children.keys(), key=lambda
+                                    k: children[k].get_data())
 
     def build_tree(self, g, root):
         """"""
         legal_moves = set([move for move in range(7) if not g.is_col_full(move)])
-        for i in range(1000):
+        for i in range(2000):
             self.build_branch(g, root, legal_moves, root.get_depth(), 42)
+            # Save the best choice every 20 branches
+            if i % 20 == 0:
+                children = self.__cur_node.get_children()
+                best_move = self.set_next_best_move(children)
 
     def build_branch(self, g, node, legal_moves, depth, x):
         """"""
@@ -102,6 +119,10 @@ class AI:
             legal_moves.add(chosen_col)
 
         return result
+    
+    def get_next_move(self):
+        """"""
+        return self.__next_move
         
 if __name__ == "__main__":
 
