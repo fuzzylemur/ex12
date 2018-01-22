a5bbfeb06128b2782af9d39aac6ce746a7093a16
jzedaka <35489421+jzedaka@users.noreply.github.com>
2018-01-22 21:52:50 +0200

    Fighting AIs fix

--- ai.py
+++ ai.py
@@ -1,7 +1,5 @@
 from random import sample
-from copy import deepcopy
 from game import Game
-#import time
 
 class Node:
 
@@ -11,6 +9,7 @@ class Node:
         self.__children = dict()
 
     def get_data(self):
+        """"""
         return self.__data
 
     def get_children(self):
@@ -24,6 +23,11 @@ class Node:
 
     def add_child(self, col, node):
         self.__children[col] = node
+        
+    def remove_child(self, col):
+        if col in self.__children.keys():
+            del self.__children[col]
+
 
 class AI:
 
@@ -38,10 +42,9 @@ class AI:
 
     def find_legal_move(self, g, func, timeout=None):
         """"""
-        #timer = time.time()
         # Saves random move as our move 
         # and then improves it until the timeout
-        legal_moves = set([move for move in range(7) if not g.is_col_full(move)])
+        legal_moves = self.find_legal_moves(g)
         self.__next_move = sample(legal_moves, 1)[0]
 
         try:
@@ -50,28 +53,41 @@ class AI:
                 children = self.__cur_node.get_children()
                 self.__cur_node = children[last_col[1]]
             # Build tree and improve current decision
-            self.build_tree(g, self.__cur_node)
+            self.build_tree(g, self.__cur_node, legal_moves)
 
         finally:
             # Update the cur node and returns the best move we found so far
             self.__cur_node = self.__cur_node.get_children()[self.__next_move]
-            print('move is '+str(self.__next_move))
+            
             func(self.__next_move)
-    
-    def set_next_best_move(self, children):
+            
+    def find_legal_moves(self, g):
+        """Finds all current possible moves and
+        deletes illegal moves from the current_node
+        children"""
+        legal_moves = set()
+        for move in range(7):
+            if not g.is_col_full(move):
+                legal_moves.add(move)
+            else:
+                self.__cur_node.remove_child(move)
+                
+        return legal_moves
+        
+    def set_next_best_move(self, children, legal_moves):
         """"""
         self.__next_move = max(children.keys(), key=lambda
                                     k: children[k].get_data())
 
-    def build_tree(self, g, root):
+    def build_tree(self, g, root, legal_moves):
         """"""
-        legal_moves = set([move for move in range(7) if not g.is_col_full(move)])
-        for i in range(1000): # Can be while true / according to timeout
+        for i in range(2000): # Can be while true / according to timeout
             self.build_branch(g, root, legal_moves)
             # Save the best choice every 20 branches
             if i % 20 == 0: # ask Gil about the exact number
                 children = self.__cur_node.get_children()
-                self.set_next_best_move(children)
+
+                self.set_next_best_move(children, legal_moves)
 
     def build_branch(self, g, node, legal_moves):
         """"""
@@ -84,16 +100,17 @@ class AI:
                 node.set_data(0)
             else:
                 node.set_data(-1)
-                
+
             g.set_game_on()
             return node.get_data()
+
         else:
             chosen_col = sample(legal_moves, 1)[0]
             temp = g.get_last_coord()
             g.make_move(chosen_col)
             if g.is_col_full(chosen_col):
                 legal_moves.remove(chosen_col)
-
+                
             if chosen_col in node.get_children().keys():
                 next_node = node.get_children()[chosen_col]
                 result = self.build_branch(g, next_node, legal_moves)
@@ -108,11 +125,7 @@ class AI:
             legal_moves.add(chosen_col)
 
         return result
-    
-    def get_next_move(self):
-        """"""
-        return self.__next_move
-        
+
 if __name__ == "__main__":
 
     ai = AI(0)
